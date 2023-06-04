dc7042b99728dcd848f4103c775f0eb4d1173b14
bb <bb@bb.com>
2023-06-05 00:37:11 +0800

    pro

--- deployments/finished.sh
+++ deployments/finished.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker login  -u developer -p Fh7FSgEQ6hMn8MB7Q4URnh76eEDdIXAg https://docker.biyong001.com/
+docker login  -u flct -p flctEHAcRqW12 https://docker.flct.io/
 
 if [ "${EVN}" = "pre" ];then
-    docker tag pre-phoenix-developer:${VERSION}-${BUILD_ID} docker.biyong001.com/pre/phoenix-developer:${VERSION}-${BUILD_ID}
-    docker push docker.biyong001.com/pre/phoenix-developer:${VERSION}-${BUILD_ID}
+    docker tag pre-phoenix-developer:${VERSION}-${BUILD_ID} docker.flct.io/flct/pre-phoenix-developer:${VERSION}-${BUILD_ID}
+    docker push docker.flct.io/flct/pre-phoenix-developer:${VERSION}-${BUILD_ID}
 else
-    docker tag phoenix-developer:${VERSION}-${BUILD_ID} docker.biyong001.com/phoenix/phoenix-developer:${VERSION}-${BUILD_ID}
-    docker push docker.biyong001.com/phoenix/phoenix-developer:${VERSION}-${BUILD_ID}
+    docker tag phoenix-developer:${VERSION}-${BUILD_ID} docker.flct.io/flct/phoenix-developer:${VERSION}-${BUILD_ID}
+    docker push docker.flct.io/flct/phoenix-developer:${VERSION}-${BUILD_ID}
 fi
